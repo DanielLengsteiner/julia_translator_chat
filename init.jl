6bd5b959f6a0b854eef7cc5ee630d2ee91d361ca
Daniel Lengsteiner <dlengsteiner@student.tgm.ac.at>
2024-03-02 19:44:29 +0100

    Add functionality to write and save messages with the language included in the URL

--- init.jl
+++
@@ -1,3 +0,0 @@
-using Genie
-
-using SearchLight, SearchLightSQLite
--- translator-chat/routes.jl
+++ translator-chat/routes.jl
@@ -1,7 +1,10 @@
-using Genie.Router
+using Genie.Router, Genie.Requests, Genie.Renderer.Json
+import Genie.Renderer.Json: json
+using GenieFramework
 using SearchLight, SearchLightSQLite
 using JSON
-import Genie.Renderer.Json: json
+using HTTP
+using TranslatorChat.Messages
 
 SearchLight.Configuration.load() |> SearchLight.connect
 
@@ -9,7 +12,7 @@ route("/") do
   serve_static_file("welcome.html")
 end
 
-route("/chat") do
+route("/messages", method = GET) do
   df = SearchLight.query("SELECT * FROM messages")
 
   sprev = ""
@@ -19,6 +22,46 @@ route("/chat") do
       sprev = sprev * ","
     end
   end
-  s = "[$(sprev)]" 
-  JSON.parse(s) |> json
+  JSON.parse("[$(sprev)]") |> json
+end
+
+mutable struct Lang
+  lang::String
+end
+
+l = Lang("")
+
+route("chat/:language") do 
+  model = @init
+  @show model
+
+  l.lang = params(:language)
+
+  page(model, formui()) |> html
+end
+
+@genietools
+
+# reactive code
+@app begin
+    @in send = false
+    @in message = ""
+    @in data = ""
+    #@in lang = ""
+    @out chat = ""
+
+    @onchange message begin
+        data = "soos " * message
+    end
+
+    @onbutton send begin
+      SearchLight.save(Message(lang = l.lang, content = message))
+    end
+end
+
+function formui()
+  cell([
+      textfield("Enter your message", :message),
+      btn("Send", @click(:send))
+  ])
 end
